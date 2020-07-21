187fdc0c53addaad1dfcecaf9bcd6fb45ef45091
Lucas Reichert <lucas.reichert@senior.com.br>
2020-07-21 17:24:11 -0300

    first commit

---
+++ src/routes.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import UsersList from './views/users/UsersList';
+import Login from './views/auth/Login';
+
+export default () => {
+    return (
+        <Switch>
+            <Route exact path="/login" component={Login}></Route>
+            <Route exact path="/users" component={UsersList}></Route>
+        </Switch>
+    );
+}
